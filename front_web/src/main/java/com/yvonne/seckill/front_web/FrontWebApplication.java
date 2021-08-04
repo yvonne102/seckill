fae17765d735bd8cfe487c06a8d60844c796a083
yvonne102 <86945792+yvonne102@users.noreply.github.com>
2021-08-04 13:33:59 +0800

    diyici

---
+++ front_web/src/main/java/com/yvonne/seckill/front_web/FrontWebApplication.java
@@ -0,0 +1,13 @@
+package com.yvonne.seckill.front_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontWebApplication.class, args);
+    }
+
+}
---
+++ front_web/src/test/java/com/yvonne/seckill/front_web/FrontWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvonne.seckill.front_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class FrontWebApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
